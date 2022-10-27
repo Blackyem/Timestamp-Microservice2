de22ef877cce08048615f0361835d214cb1c01aa
yelpcamp-profile <kolayem44@gmail.com>
2022-10-27 02:17:02 +0100

    add update

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 // index.js
 // where your node app starts
-
 // init project
 var express = require('express');
+const dotenv = require("dotenv");
+
 var app = express();
+
+dotenv.config();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({
+  optionsSuccessStatus: 200
+})); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -20,9 +25,29 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/timestamp/:timestamp", (req, res) => {
+  let timestamp = req.params.timestamp;
+  if (timestamp.match(/\d{5,}/)) {
+    timestamp = +timestamp;
+  }
+  const date = new Date(timestamp);
+  if (date.toUTCString() == "Invalid Date") {
+    res.json({
+      error: date.toUTCString()
+    })
+  }
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  })
 });
+app.get("/api/timestamp", (req, res) => {
+  let date = new Date()
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  })
+})
 
 
 
